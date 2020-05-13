b187fb7aa532062b98ab1db29a411ae3e1145ebf
Eudson Durães <eudson.duraes@gmail.com>
2020-05-13 10:07:56 -0300

    Adicionado icone no botão de alterar no perfil e alterando layout do perfil

--- SistemaAcademia/resources/views/MeuPerfilPersonal.blade.php
+++ SistemaAcademia/resources/views/MeuPerfilPersonal.blade.php
@@ -6,35 +6,53 @@ Perfil
 
 @section('conteudo')
 <h1>Meu perfil</h1>
-        <table class='TablePerfil table py-5'>
+        <table class='TablePerfil table mt-5'>
             <tr>
             <td>Nome:</td><td>{{$usuario->name}}</td><td><button type="button" class="btn btn-dark" data-toggle="modal" data-target="#AlterarNome">
-              Alterar
+              <svg class="bi bi-pencil" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M11.293 1.293a1 1 0 011.414 0l2 2a1 1 0 010 1.414l-9 9a1 1 0 01-.39.242l-3 1a1 1 0 01-1.266-1.265l1-3a1 1 0 01.242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z" clip-rule="evenodd"/>
+              <path fill-rule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 00.5.5H4v.5a.5.5 0 00.5.5H5v.5a.5.5 0 00.5.5H6v-1.5a.5.5 0 00-.5-.5H5v-.5a.5.5 0 00-.5-.5H3z" clip-rule="evenodd"/>
+              </svg>
             </button></td>
             </tr>
             <tr>
             <td>Email:</td><td>{{$usuario->email}}</td><td><button type="button" class="btn btn-dark" data-toggle="modal" data-target="#AlterarEmail">
-              Alterar
+              <svg class="bi bi-pencil" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M11.293 1.293a1 1 0 011.414 0l2 2a1 1 0 010 1.414l-9 9a1 1 0 01-.39.242l-3 1a1 1 0 01-1.266-1.265l1-3a1 1 0 01.242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z" clip-rule="evenodd"/>
+              <path fill-rule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 00.5.5H4v.5a.5.5 0 00.5.5H5v.5a.5.5 0 00.5.5H6v-1.5a.5.5 0 00-.5-.5H5v-.5a.5.5 0 00-.5-.5H3z" clip-rule="evenodd"/>
+              </svg>
             </button></td>
             </tr>
             <tr>
             <td>Sexo:</td><td>{{$usuario->sexo}}</td><td><button type="button" class="btn btn-dark" data-toggle="modal" data-target="#AlterarSexo">
-              Alterar
+              <svg class="bi bi-pencil" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M11.293 1.293a1 1 0 011.414 0l2 2a1 1 0 010 1.414l-9 9a1 1 0 01-.39.242l-3 1a1 1 0 01-1.266-1.265l1-3a1 1 0 01.242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z" clip-rule="evenodd"/>
+              <path fill-rule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 00.5.5H4v.5a.5.5 0 00.5.5H5v.5a.5.5 0 00.5.5H6v-1.5a.5.5 0 00-.5-.5H5v-.5a.5.5 0 00-.5-.5H3z" clip-rule="evenodd"/>
+              </svg>
             </button></td>
             </tr>
             <tr>
            <td>Data de nascimento:</td><td>{{$usuario->nascimento}}</td><td><button type="button" class="btn btn-dark" data-toggle="modal" data-target="#AlterarData">
-             Alterar
+             <svg class="bi bi-pencil" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+             <path fill-rule="evenodd" d="M11.293 1.293a1 1 0 011.414 0l2 2a1 1 0 010 1.414l-9 9a1 1 0 01-.39.242l-3 1a1 1 0 01-1.266-1.265l1-3a1 1 0 01.242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z" clip-rule="evenodd"/>
+             <path fill-rule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 00.5.5H4v.5a.5.5 0 00.5.5H5v.5a.5.5 0 00.5.5H6v-1.5a.5.5 0 00-.5-.5H5v-.5a.5.5 0 00-.5-.5H3z" clip-rule="evenodd"/>
+             </svg>
            </button></td>
             </tr>
             <tr>
               <td>Esporte Principal</td><td>{{$usuario->AtividadeFisica}}</td><td><button type="button" class="btn btn-dark" data-toggle="modal" data-target="#AlterarAtividadeFisica">
-                Alterar
+                <svg class="bi bi-pencil" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M11.293 1.293a1 1 0 011.414 0l2 2a1 1 0 010 1.414l-9 9a1 1 0 01-.39.242l-3 1a1 1 0 01-1.266-1.265l1-3a1 1 0 01.242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z" clip-rule="evenodd"/>
+                <path fill-rule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 00.5.5H4v.5a.5.5 0 00.5.5H5v.5a.5.5 0 00.5.5H6v-1.5a.5.5 0 00-.5-.5H5v-.5a.5.5 0 00-.5-.5H3z" clip-rule="evenodd"/>
+                </svg>
               </button></td>
             </tr>
             <tr>
               <td>Telefone:</td><td>{{$usuario->telefone}}</td><td><button type="button" class="btn btn-dark" data-toggle="modal" data-target="#AlterarTelefone">
-                Alterar
+                <svg class="bi bi-pencil" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M11.293 1.293a1 1 0 011.414 0l2 2a1 1 0 010 1.414l-9 9a1 1 0 01-.39.242l-3 1a1 1 0 01-1.266-1.265l1-3a1 1 0 01.242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z" clip-rule="evenodd"/>
+                <path fill-rule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 00.5.5H4v.5a.5.5 0 00.5.5H5v.5a.5.5 0 00.5.5H6v-1.5a.5.5 0 00-.5-.5H5v-.5a.5.5 0 00-.5-.5H3z" clip-rule="evenodd"/>
+                </svg>
               </button></td>
             </tr>
             <tr>
@@ -153,6 +171,34 @@ Perfil
         @csrf
         <label for="Atividade Fisica">Atividade física principal</label>
         <input class="form-control" name="AtividadeFisica" type="text" id="Atividade Fisica" value="{{$usuario->AtividadeFisica}}">
+        <!-- Enviando id do usuário através de um input oculto -->
+        <input type='hidden' name="id" value="{{$usuario->id}}">
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+        <button type="submit" class="btn btn-primary">Salvar alteração</button>
+      </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<!-- Modal Data de nascimento -->
+<div class="modal fade" id="AlterarData" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Alterar data de nascimento</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+
+      <form class="form-group" action="{{route('AlterarPersonal')}}" method="post">
+        @csrf
+        <label for="Nascimento">Data de nascimento:</label>
+        <input class="form-control" name="Nascimento" type="date" id="Nascimento">
         <!-- Enviando id do usuário através de um input oculto -->
         <input type='hidden' name="id" value="{{$usuario->id}}">
       </div>
